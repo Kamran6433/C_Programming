fbb985686c57a934b15a68bddf5864d380692ad7
kamran6433 <kamran_12@icloud.com>
2022-06-28 12:59:40 +0100

    Fahrenheit to Celcius conversion program.

--- Main.c
+++ Main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
-int main() {
-  printf("Hello World. Here i am again, again!!!");
-  return 0;
-} 
+int main(void) {
+
+  int fahrenheit, celcius;
+  int lower, upper, step;
+
+  lower = 0;
+  upper = 300;
+  step = 20;
+
+  fahrenheit = lower;
+  printf("Fahrenheit: \t Celcius:\n");
+  while (fahrenheit <= upper) {
+    celcius = 5 * (fahrenheit - 32) / 9;
+    printf("%d \t\t %d \n", fahrenheit, celcius);
+    fahrenheit = fahrenheit + step;
+  }
+}
