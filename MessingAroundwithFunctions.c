a013bf171b73b6114841377fefceb338613e99bd
kamran6433 <kamran_12@icloud.com>
2022-06-29 17:10:44 +0100

    Created another function for the function file.

--- MessingAroundwithFunctions.c
+++ MessingAroundwithFunctions.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 int power(int a, int b);
-int timesByItself();
+int timesByItself(int a[]);
 int dividingBy5();
 int helloWorld();
 
 int main(void) {
 
-
+    int numbers[5] = {1, 2, 3, 4, 5};
 
 }
 
@@ -26,5 +26,14 @@ int power(int a, int b) {
     }
 
     return power;
+
+}
+
+int timesByItself(int a[]) {
+
+    for (int i = 0; i < sizeof(a); i++) {
+        a[i] = a[i] * a[i];
+    }
+    
 
 }
