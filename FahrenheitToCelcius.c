2d81a00d0b52604588489d72f46bb26c5b183415
kamran6433 <kamran_12@icloud.com>
2022-06-29 17:21:41 +0100

    Done the same for the other file.

--- FahrenheitToCelcius.c
+++ FahrenheitToCelcius.c
@@ -1,9 +1,30 @@
 #include <stdio.h>
 
+int conversion(float fahrenheit, float celcius, int lower, int upper, int step);
+
 int main(void) {
 
   float fahrenheit, celcius;
   int lower, upper, step;
+
+  printf("%d", conversion(fahrenheit, celcius, lower, upper, step));
+
+  // lower = 0;
+  // upper = 300;
+  // step = 20;
+
+  // fahrenheit = lower;
+  // printf("Fahrenheit: \t Celcius:\n");
+  
+  // while (fahrenheit <= upper) {
+  //   celcius = (5.0/9.0) * (fahrenheit - 32.0);
+  //   printf("%0.0f \t\t %0.2f \n", fahrenheit, celcius);
+  //   fahrenheit = fahrenheit + step;
+  // }
+
+}
+
+int conversion(float fahrenheit, float celcius, int lower, int upper, int step) {
 
   lower = 0;
   upper = 300;
@@ -16,6 +37,6 @@ int main(void) {
     celcius = (5.0/9.0) * (fahrenheit - 32.0);
     printf("%0.0f \t\t %0.2f \n", fahrenheit, celcius);
     fahrenheit = fahrenheit + step;
-
   }
+
 }
