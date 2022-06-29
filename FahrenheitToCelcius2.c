3caf79027972a8689974981e7795446cdb8a32c2
kamran6433 <kamran_12@icloud.com>
2022-06-29 17:17:15 +0100

    rewrote the temperature conversion program to use a function for conversion.

--- FahrenheitToCelcius2.c
+++ FahrenheitToCelcius2.c
@@ -4,12 +4,25 @@
 #define UPPER 300
 #define STEP 20
 
+int conversion(int a);
+
 int main(void) {
 
-int fahrenheit;
+    int a;
+    printf("%d", conversion(a));
 
-for (fahrenheit = LOWER; fahrenheit <= UPPER; fahrenheit = fahrenheit + STEP) {
+// int fahrenheit;
+
+// for (fahrenheit = LOWER; fahrenheit <= UPPER; fahrenheit = fahrenheit + STEP) {
+//     printf("%3d %6.1f\n", fahrenheit, (5.0/9.0)*(fahrenheit - 32));
+//     }
+
+} 
+
+int conversion(int fahrenheit) {
+
+    for (fahrenheit = LOWER; fahrenheit <= UPPER; fahrenheit = fahrenheit + STEP) {
     printf("%3d %6.1f\n", fahrenheit, (5.0/9.0)*(fahrenheit - 32));
     }
 
-} 
+}
