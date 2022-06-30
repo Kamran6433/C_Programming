7b0796148892bc4d73405c78bb93a2501c18bf4a
kamran6433 <kamran_12@icloud.com>
2022-07-01 00:28:10 +0100

    Updated version of CharacterArray.c.

--- CharacterArrays.c
+++ CharacterArrays.c
@@ -1,27 +1,32 @@
 #include <stdio.h>
+
 #define MAXLINE 1000
 
-int getLine(char line[], int maxLine);
-void copy(char to[], char from[]);
+int max; // Maximum length seen so far.
+char line[MAXLINE]; // Current input line.
+char longest[MAXLINE]; // Longest line saved here.
+
+int getLine(void);
+void copy(void);
 
 // Prints the longest input line.
 main() {
 
     int length; // Current line length.
-    int max; // Maximum length seen so far.
-    char line[MAXLINE]; // Current input line.
-    char longest[MAXLINE]; // Longest line saved here.
+    extern int max;
+    extern char longest[];
 
     max = 0;
-    while ((length = getLine(line, MAXLINE)) > 0) {
+    while ((length = getLine()) > 0) {
 
         if (length > max) {
             max = length;
-            copy(longest, line);
+            copy();
         }
 
     }
 
+    // If there was a line.
     if (max > 0) {
         printf("Longest: %s",longest);
         printf("Length of input: %d", strlen(longest));
