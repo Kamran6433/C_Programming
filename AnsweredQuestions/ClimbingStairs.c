1040b89c120c904101f2f77d8818760af424e0aa
kamran6433 <kamran_12@icloud.com>
2022-06-30 15:59:00 +0100

    Moved the question into the AnsweredQuestion file.

---
+++ AnsweredQuestions/ClimbingStairs.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+// This is a dynamic programming question.
+int climbStairs(int n);
+
+main() {
+
+    int input = 10;
+    printf("Answer: %d", climbStairs(input));
+
+}
+
+int climbStairs(int n){
+
+    int dynamicProgramming[n + 1];
+
+    dynamicProgramming[0] = 1;
+    dynamicProgramming[1] = 1;
+
+    for (int i = 2; i <= n; ++i) {
+        dynamicProgramming[i] = dynamicProgramming[i - 1] + dynamicProgramming[i - 2];
+    }
+    return dynamicProgramming[n];
+
+}
