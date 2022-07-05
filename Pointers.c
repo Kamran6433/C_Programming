60a2e9788c488458922ec21a802064e8c4f96d52
kamran6433 <kamran_12@icloud.com>
2022-07-05 12:33:38 +0100

    Added more examples to understand pointers.

--- Pointers.c
+++ Pointers.c
@@ -37,12 +37,15 @@ int var = 10, *pointer4 = &var;
 int main(void) {
 
     // This will be used to tinker around pointer to get a better understanding of pointers.
-    printf("*Pointer4 (Grabbing the VALUE of the pointer): %d\n", *pointer4);
+    printf("\n*Pointer4 (Grabbing the VALUE of the pointer): %d\n", *pointer4);
     printf("var (Grabbing the VALUE of var): %d\n", var);
-    printf("Here the values are the same because var is 10 and pointer4 points to var's value.\n");
+    printf("Here the values are the same because var is 10 and pointer4 points to var's value.\n\n");
     printf("&Pointer4 (Grabbing the ADDRESS of the pointer): %d\n", &pointer4);
     printf("&var (Grabbing the ADDRESS of var): %d\n", &var);
-    printf("As you can see here, the address of pointer4 and var are directly next to (Above or below in the stack) each other.\n");
-
+    printf("As you can see here, the address of pointer4 and var are directly next to (Above or below in the stack) each other.\n\n");
+    *pointer4 = 99;
+    printf("*pointer4 = 99;\n");
+    printf("printf(%%d, var) = %d", var);
+    printf("As you can see here, I have changed the value of the var that pointer4 points to through the pointer itself.\n\n");
 
 }
