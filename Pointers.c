f2b6560d170cab5d782eed41e2cd17136aa0ddd0
kamran6433 <kamran_12@icloud.com>
2022-07-05 12:18:32 +0100

    Tinkering and experimenting.

--- Pointers.c
+++ Pointers.c
@@ -39,7 +39,10 @@ int main(void) {
     // This will be used to tinker around pointer to get a better understanding of pointers.
     printf("*Pointer4 (Grabbing the VALUE of the pointer): %d\n", *pointer4);
     printf("var (Grabbing the VALUE of var): %d\n", var);
+    printf("Here the values are the same because var is 10 and pointer4 pointss to var's value.\n");
     printf("&Pointer4 (Grabbing the ADDRESS of the pointer): %d\n", &pointer4);
     printf("&var (Grabbing the ADDRESS of var): %d\n", &var);
+    printf("As you can see here, the address of pointer4 and var are directly next to (Above or below in the stack) each other.\n");
+
 
 }
