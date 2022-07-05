5c8a3b04e7b202e53ab7466635571080792e3a3d
kamran6433 <kamran_12@icloud.com>
2022-07-05 12:26:20 +0100

    Added a screenshot.

--- Pointers.c
+++ Pointers.c
@@ -25,8 +25,8 @@ float z = 2.4;
 int var = 10, *pointer4 = &var;
 // DATA TYPE: int   DATA TYPE: int
 // NAME: var        NAME: pointer4
-// VALUE: 10        VALUE: n
-// ADDRESS: n       ADDRESS: m
+// VALUE: 10        VALUE: 10000
+// ADDRESS: 10000   ADDRESS: 10004
 //** USE POINTERSPIC.JPEG FOR A PICTORIAL REPRESENTATION OF ^ **//
 
 // *pointer4 means Value of pointer4.
@@ -39,7 +39,7 @@ int main(void) {
     // This will be used to tinker around pointer to get a better understanding of pointers.
     printf("*Pointer4 (Grabbing the VALUE of the pointer): %d\n", *pointer4);
     printf("var (Grabbing the VALUE of var): %d\n", var);
-    printf("Here the values are the same because var is 10 and pointer4 pointss to var's value.\n");
+    printf("Here the values are the same because var is 10 and pointer4 points to var's value.\n");
     printf("&Pointer4 (Grabbing the ADDRESS of the pointer): %d\n", &pointer4);
     printf("&var (Grabbing the ADDRESS of var): %d\n", &var);
     printf("As you can see here, the address of pointer4 and var are directly next to (Above or below in the stack) each other.\n");
