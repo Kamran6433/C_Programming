1e90b565775df09c418ffaccc8ebe7555e45d177
kamran6433 <kamran_12@icloud.com>
2022-07-11 12:58:41 +0100

    Quick save.

---
+++ KilometerToMile.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+float *conversionK2M(float kilometer);
+float *conversionM2K(float mile);
+
+float *conversionK2M(float kilometer) {
+
+        float answer = kilometers * 0.621371;
+        return &answer;
+
+}
+
+float *conversionM2K(float mile) {
+
+    float answer = 
+
+}
+
+main(void) {
+
+
+
+}
