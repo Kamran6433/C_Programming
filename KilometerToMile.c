2ff98b49286adc67f0820c5c869ae72dc0457bdd
kamran6433 <kamran_12@icloud.com>
2022-07-11 14:22:56 +0100

    Quick save.

--- KilometerToMile.c
+++ KilometerToMile.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
 
+#define KILOMETER 1.60934
+#define MILE 0.621371
+
 float *conversionK2M(float kilometer);
 float *conversionM2K(float mile);
+float *answer1;
+float *answer2;
 
 float *conversionK2M(float kilometer) {
 
-    float kilo = kilometer * 0.621371;
-    return &kilo;
+    kilometer * MILE;
+    return &kilometer;
 
 }
 
 float *conversionM2K(float mile) {
 
-    float mil = mile * 1.60934;
-    return &mil;
+    mile * KILOMETER;
+    return &mile;
 
 }
 
@@ -22,8 +27,8 @@ main(void) {
     float k = 10.0;
     float m = 100.0;
 
-    float *answer1 = conversionK2M(k);
-    float *answer2 = conversionM2K(m);
+    *answer1 = conversionK2M(k);
+    *answer2 = conversionM2K(m);
 
     printf("Kilometers %f to miles: %f\n", k, *answer1);
     printf("miles %f to kilometers: %f\n", m, *answer2);
