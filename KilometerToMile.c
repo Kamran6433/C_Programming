4c42d186aa9ed2114b361b7454d0cb04d138b8a8
kamran6433 <kamran_12@icloud.com>
2022-07-13 11:50:13 +0100

    Quick save.

--- KilometerToMile.c
+++ KilometerToMile.c
@@ -5,8 +5,6 @@
 
 float *conversionK2M(float kilometer);
 float *conversionM2K(float mile);
-float *answer1;
-float *answer2;
 
 float *conversionK2M(float kilometer) {
 
@@ -27,8 +25,8 @@ main(void) {
     float k = 10.0;
     float m = 100.0;
 
-    *answer1 = conversionK2M(k);
-    *answer2 = conversionM2K(m);
+    float *answer1 = conversionK2M(k);
+    float *answer2 = conversionM2K(m);
 
     printf("Kilometers %f to miles: %f\n", k, *answer1);
     printf("miles %f to kilometers: %f\n", m, *answer2);
