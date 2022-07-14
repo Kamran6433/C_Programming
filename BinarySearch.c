5ae709cca5ae8eed07dbcd64f38994ae2a2307bd
kamran6433 <kamran_12@icloud.com>
2022-07-14 14:46:40 +0100

    Working on Binary search in C.

---
+++ BinarySearch.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+// This algorithm has O(logn) time complexity becuase the search is halved eahc iteration.
+int binarySearch(int target, int array[], int n) {
+
+    int low, mid, high;
+
+    low = 0;
+    high = n - 1;
+
+    while (low <= high) {
+
+        mid = (low + high) / 2;
+        if (target < array[mid]) {
+            high = mid -1;
+        }
+
+    }
+
+
+}
