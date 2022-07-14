e4db4b016c0ff9a0728bb52ee8f67f34aab12d77
kamran6433 <kamran_12@icloud.com>
2022-07-14 16:40:28 +0100

    Learnt something new about a for loop.

--- ForLoop.c
+++ ForLoop.c
@@ -4,7 +4,7 @@ main() {
 
     // Here expression 1 and expression 3 are omitted from the loop and expression 2 (relational expression) is not included which means the for loop is permanently true and needs to be broken in a different way. Therefore the for loop below is an infinite for loop.
     for (;;) {
-
+        printf("If you're reading this, get to work!");
     }
 
 }
