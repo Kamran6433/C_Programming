9c623cdabe2be84b36ff4d173eb1204761e4de9b
kamran6433 <kamran_12@icloud.com>
2022-07-14 15:31:30 +0100

    Learnt something new about a for loop.

---
+++ ForLoop.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+main() {
+
+    // Here expression 1 and expression 3 are omitted from the loop and expression 2 (relational expression) is not included which means the for loop is permanently true and needs to be broken in a different way. Therefore the for loop below is an infinite for loop.
+    for (;;) {
+
+    }
+
+}
