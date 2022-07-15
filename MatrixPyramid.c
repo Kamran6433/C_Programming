7c4fc46ba7a4f92f6412ebe6324bf52a617c117d
kamran6433 <kamran_12@icloud.com>
2022-07-15 17:25:16 +0100

    Pyramid with stars created using matrices.

--- MatrixPyramid.c
+++ MatrixPyramid.c
@@ -4,13 +4,13 @@ int main(void) {
 
     int rows, columns, n;
     printf("How many rows do you want?\n");
-    scanf(%d, &n);
+    scanf("%d", &n);
 
     for (rows = 1; rows < n; rows++) {
 
         for (columns = 1; columns <= 2 * n -1; columns++) {
 
-            if (columns >= n - (rows - 1) && columns <= n + (i - 1)) {
+            if (columns >= n - (rows - 1) && columns <= n + (rows - 1)) {
                 printf("*");
             }
             else {
