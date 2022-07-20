3dbfdaa6e7cf27801e32efacfa57281c0a20af18
kamran6433 <kamran_12@icloud.com>
2022-07-20 12:35:30 +0100

    Learning Structures in C.

--- Structures.c
+++ Structures.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 // A structure is a user defined data type that can be used to group elements of 
-// different types together into a single type.
+// different types together into a SINGLE TYPE.
 
 // Structure gives us the power to define our own data types.
 
@@ -20,6 +20,19 @@
 // Feul Tank Capacity: 35
 // Seating Capacity: 5
 // City Milage: 22.54 kmpl
+
+// As you can see here i've created my own data type.
+// This is similar to defining an integer - int x, y, z;
+// so - struct {everything inside} car1, car2, car3;
+struct {
+
+    char *engine;
+    char *fuelType;
+    int fuelTankCap;
+    int seatingCap;
+    float cityMileage;
+
+} car1, car2;
 
 int main(void) {
     
