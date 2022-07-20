c6c0cacab5c69ce62693f7b7c1696f7296606da6
kamran6433 <kamran_12@icloud.com>
2022-07-20 12:23:58 +0100

    Learning Structures in C.

--- Structures.c
+++ Structures.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
+// Car 1 specifications:
+
+// Engine: DDis 190 Engine
+// Fuel Type: Petrol
+// Feul Tank Capacity: 37
+// Seating Capacity: 5
+// City Milage: 19.74 kmpl
+
 int main(void) {
     
-    
+
 }
