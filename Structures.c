7bf3e02020554791a5e40c87de25655c32eb03de
kamran6433 <kamran_12@icloud.com>
2022-07-20 13:11:01 +0100

    Learning structures.

--- Structures.c
+++ Structures.c
@@ -23,7 +23,7 @@
 
 // As you can see here i've created my own data type.
 // This is similar to defining an integer - int x, y, z;
-// so - struct {everything inside} car1, car2, car3;
+// so - struct {everything inside} car1, car2;
 // It is completely fine to not name the structure however if you 
 // want to use the same structure again you'll have to write it out again 
 // unless you've given it a name.
@@ -53,5 +53,13 @@ int main(void) {
     // This structure is the same car structure but declared locally 
     // (Only availabe within this main function).
     struct car bmw;
+    // Structures work very similarly to objects.
+    // Here I declared a car structure called bugatti and initilaised it with its values.
+    struct car bugatti = {"V10 Engine", "Petrol", 40, 2, 6.3};
+    printf("bugatti Engine: %s\n", bugatti.engine);
+    printf("bugatti Fuel: %s\n", bugatti.fuelType);
+    printf("bugatti Fuel capacity: %d\n", bugatti.fuelTankCap);
+    printf("bugatti Seating capacity: %d\n", bugatti.seatingCap);
+    printf("bugatti city mileage: %0.2fkmpl\n", bugatti.cityMileage);
     
 }
