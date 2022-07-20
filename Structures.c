d5fd0cf8aed8b7538c6da88fafbeda7572c5e913
kamran6433 <kamran_12@icloud.com>
2022-07-20 12:25:39 +0100

    Learning Structures in C.

--- Structures.c
+++ Structures.c
@@ -8,6 +8,14 @@
 // Seating Capacity: 5
 // City Milage: 19.74 kmpl
 
+// Car 2 specifications:
+
+// Engine: 1.2L Kappa Dual VTVT
+// Fuel Type: Diesel
+// Feul Tank Capacity: 35
+// Seating Capacity: 5
+// City Milage: 22.54 kmpl
+
 int main(void) {
     
 
