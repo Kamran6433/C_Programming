aa3fcf5cdaf3394875dd55f69b06a785aeade0f7
kamran6433 <kamran_12@icloud.com>
2022-07-21 11:34:44 +0100

    Added all the macros for tinkering.

--- MacrosAndConstants.c
+++ MacrosAndConstants.c
@@ -1,4 +1,4 @@
-// Anything that has a '#' as a prefix, is a macro.
+// Anything that has a '#' preceding it, is a macro.
 // This is a macro.
 #include <stdio.h>
 // This is also a macro.
@@ -6,3 +6,6 @@
 // This is a function that is also a macro.
 #define SWAP(A, B) A ^= B ^= A ^= B
 // NOTE: All of these are constants as the user isn't able to change the values of these constants(macros).
+// In the example below I am using '#' to get the parameters replaced by the actual 
+// arguments once this code runs.  
+#define MULTIPLY(X, Y) printf(#X "*" #Y "is %d\n", X * Y)
