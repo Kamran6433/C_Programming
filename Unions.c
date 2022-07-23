bc12c91a4866ac833793369bcc8c2efcd7c79ea4
kamran6433 <kamran_12@icloud.com>
2022-07-23 14:19:15 +0100

    Learning Unions.

--- Unions.c
+++ Unions.c
@@ -10,8 +10,10 @@ union example {
 
 int main(void) {
 
+    // Union is a user defined data type similar to structures however,
+    // all the members and the union share the smae memory location.
     union example example = {2, "a", 1.2};
     printf("Address of x: %p, Address of y: %p, Address of z: %p\n", &example.x, &example.y, &example.z);
-    printf("As you can see, once the program is executed, all the members of the union share the exact same address.");
+    printf("As you can see, once the program is executed, all the members of the union share the exact same memory location: %p", &example);
 
 }
