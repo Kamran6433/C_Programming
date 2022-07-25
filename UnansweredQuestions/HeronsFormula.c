c4c83faaa61730ba64f31259f045961198bc3a68
kamran6433 <kamran_12@icloud.com>
2022-07-25 13:36:56 +0100

    Working on programming questions.

---
+++ UnansweredQuestions/HeronsFormula.c
@@ -0,0 +1,16 @@
+// Write function heron which calculates the area of a triangle with sides a, b, and c (x, y, z in COBOL).
+
+// Heron's formula:
+
+// \sqrt{s * (s - a) * (s - b) * (s - c)} 
+ 
+// where
+
+// s=(a + b + c)/2​
+ 
+// Output should have 2 digits precision.
+float heron(int a, int b, int c) {
+
+    //  <----  hajime!
+
+}
