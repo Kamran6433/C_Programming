fef9c0971adc29002a59bd25c9ac3c96ef6bb879
kamran6433 <kamran_12@icloud.com>
2022-07-25 12:05:52 +0100

    Working on programming questions.

--- UnansweredQuestions/StringEndsWith?.c
+++ UnansweredQuestions/StringEndsWith?.c
@@ -5,32 +5,55 @@
 
 bool solution(const char *string, const char *ending) {
 
-    if (ending == "") {
+    if (strlen(ending) == 0) {
         return true;
     }
-
-    if (string == ending) {
-        return true;
-    }
-
+  
     if (strlen(string) < strlen(ending)) {
         return false;
     }
 
     char *copyOfEnding2;
     char *copyOfEnding1;
-    for (int i = strlen(string); i < ((strlen(string) + 1)) && i > ((strlen(string) - 3)); i--) {
-        copyOfEnding2 = string[i];
+    for (int i = strlen(string); i < ((strlen(string) + 1)) && i > ((strlen(string) - 2)); i--) {
+        copyOfEnding2 += string[i];
     }
 
-    for (int i = strlen(string); i < ((strlen(string) + 1)) && i > ((strlen(string) - 2)); i--) {
+    for (int i = strlen(string); i < ((strlen(string) + 1)) && i > ((strlen(string) - 1)); i--) {
         copyOfEnding1 = string[i];
     }
 
     if (copyOfEnding2 == ending || copyOfEnding1 == ending) {
         return true;
     }
+  
+    if (strlen(string) == strlen(ending) && string == ending) {
+        return true;
+    } 
 
     return false;
 
+}
+
+// Correct answer.
+
+bool solution(const char *string, const char *ending)
+{
+    int slen = strlen(string);
+    int elen = strlen(ending);
+    
+  
+    if (slen < elen) return false;
+    
+  
+    int y = 0;
+    for(int x = slen - elen; x <= slen; ++x)
+    {
+        if(string[x] != ending[y])
+        {
+          return false;
+        }
+        ++y;
+    }
+    return true;
 }
