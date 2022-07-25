800ffd8cd41368b876824530ce92bbb7fe5f65c2
kamran6433 <kamran_12@icloud.com>
2022-07-25 11:24:48 +0100

    Working on programming questions.

--- UnansweredQuestions/NumberOfPeopleInTheBus.c
+++ UnansweredQuestions/NumberOfPeopleInTheBus.c
@@ -32,3 +32,16 @@ int bus_terminus (size_t nb_stops, const short bus_stops[nb_stops][2]) {
     return bus;
 
 } 
+
+// Best version I found and the quickest.
+
+int bus_terminus (size_t nb_stops, const short bus_stops[nb_stops][2])
+{
+  auto bus = 0; // 🚌 :p
+  for(size_t i = 0; i<nb_stops; i++)
+    {
+    bus += bus_stops[i][0];
+    bus-= bus_stops[i][1];
+  }
+  return bus;
+}
