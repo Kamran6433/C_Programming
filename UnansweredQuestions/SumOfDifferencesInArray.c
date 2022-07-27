f37654ef4ec55915470d9d76c52f91b7c06f680a
kamran6433 <kamran_12@icloud.com>
2022-07-27 12:05:11 +0100

    Working on programming questions.

--- UnansweredQuestions/SumOfDifferencesInArray.c
+++ UnansweredQuestions/SumOfDifferencesInArray.c
@@ -13,14 +13,28 @@
 
 int diffsum(const int *arr, size_t n)
 {
-    int *copy;
+    int *copy = 0;
     int answer = 0;
+    int length = sizeof(arr) / sizeof(arr[0]);
     strcpy(copy, arr);
-    if (n != 0 || n != 1) {
-        for (int i = (n - 1); i > 0; i--) {
+    if (length != 0 || length != 1) {
+        for (int i = (length - 1); i > 0; i--) {
             int temp = arr[i] - arr[i - 1];
             answer += temp;
         }
     }
     return answer;
+}
+
+// Correct answer
+
+int diffsum(const int *arr, size_t n) {
+  if (n == 0)
+    return 0;
+  int min = arr[0], max = arr[0];
+  for (size_t i = 0; i < n; i++) {
+    if (arr[i] < min) min = arr[i];
+    if (arr[i] > max) max = arr[i];
+  }
+  return max - min;
 }
